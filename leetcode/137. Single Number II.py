33c535a0460d10c92d197396bf773aa5c58a1bd3
gjwei <gjwei@gjweis-iMac.local>
2018-09-05 19:55:59 +0800

    single number2

--- leetcode/137. Single Number II.py
+++ leetcode/137. Single Number II.py
@@ -4,6 +4,19 @@ def single_number2(nums):
     # Given a non-empty array of integers, every 
     # element appears three times except for one, 
     # which appears exactly once. Find that single one.
+    counts = [0 for _ in range(32)]
+    for n in nums:
+        for i in range(32):
+            counts[i] += (n >> (31 - i)) & 1
+
+    result = 0
+    for i in range(32):
+        if counts[i] % 3 != 0:
+            result += (1 << i)
+
+    return result    
+
+    
     
 
 
