07fa69137877b0a3bc6f0df7d7a190d737bacd6c
gjwei <gjwei1993@163.com>
2018-09-19 16:40:31 +0800

    update

--- written_exam/tencent/gcd.py
+++ written_exam/tencent/gcd.py
@@ -1,5 +1,28 @@
-#!/usr/bin/env python
-#-*- coding:utf-8 -*- 
-#Author: gjwei
+from functools import reduce
+from math import gcd
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
 
 
+def lcm_seq(seq):
+    return reduce(lcm, seq)
+
+
+def solution(n):
+    m = n + 1
+    l1 = lcm_seq(list(range(1, m + 1)))
+    l2 = m
+
+    while l1 != l2:
+        m += 1
+        l1 = lcm(l1, m)
+        l2 = lcm(l2, m)
+
+    return m
+
+
+if __name__ == '__main__':
+    n = int(input())
+    r = solution(n)
+    print(r)
